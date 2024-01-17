006a87f41e8ef0828c2d94f2c19da2647d599521
dounya mellouki <93975469+dounya-m@users.noreply.github.com>
2024-01-17 15:22:30 +0100

    service

--- src/app/service/course.service.ts
+++ src/app/service/course.service.ts
@@ -1,5 +1,7 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Course } from '../models/course';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,12 +11,15 @@ export class CourseService {
   http = inject(HttpClient)
   apiUrl  = 'http://localhost:3000/courses'
 
-  getMethode(){
-    return this.http.get(this.apiUrl)
+  getMethode(): Observable<Course[]>{
+    return this.http.get<Course[]>(this.apiUrl)
   }
 
   postMethode(data: any){
     this.http.post(this.apiUrl, data)
+  }
+  UpdateMethode(id: any, data: any){
+    this.http.post(this.apiUrl+'/'+id, data)
   }
 
 }
